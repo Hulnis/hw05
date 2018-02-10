734effef90d1d2c4a3d1901bb3a396a03ac34429
Hulnis <eric.parker13@gmail.com>
2018-02-10 13:41:54 -0500

    oneClicked? 3

--- lib/memory_game/game.ex
+++ lib/memory_game/game.ex
@@ -59,10 +59,7 @@ defmodule MemoryGame.Game do
   end
 
   def click_card(game, card) do
-    # oneClicked =
-    # prevCard = game[:prevCard]
-
-    if Map.fetch(game, :oneClicked) do
+    if game.oneClicked do
       if card.key === prevCard.key do
         game.oneClicked = false
         game.prevCard = nil
