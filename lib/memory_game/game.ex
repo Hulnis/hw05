a8b1813801279494835927df889e11e570206a65
Hulnis <eric.parker13@gmail.com>
2018-02-10 14:34:53 -0500

    click event

--- lib/memory_game/game.ex
+++ lib/memory_game/game.ex
@@ -19,7 +19,7 @@ defmodule MemoryGame.Game do
     |> Enum.map(fn {val, index} ->
       %{
         :value => val,
-        :state => "solved",
+        :state => "hidden",
         :key => index,
       }
     end)
@@ -57,43 +57,43 @@ defmodule MemoryGame.Game do
     IO.inspect(game)
   end
 
-  # def click_card(game, card) do
-  #   if game.oneClicked === true do
-  #     if card.key === prevCard.key do
-  #       game.oneClicked = false
-  #       game.prevCard = nil
-  #       new_card1 = %{
-  #         :value => card.value,
-  #         :state => "solved",
-  #         :key => card.key
-  #       }
-  #       new_card2 = %{
-  #         :value => prevCard.value,
-  #         :state => "solved",
-  #         :key => prevCard.key
-  #       }
-  #       Map.put(game.cards, card.key, new_card1)
-  #       Map.put(game.cards, prevCard.key, new_card2)
-  #     else
-  #       game.oneClicked = false
-  #       game.prevCard = nil
-  #       Task.async(fn -> hide_two_cards(game, card1, card2) end)
-  #       new_card = %{
-  #         :value => card.value,
-  #         :state => "revealed",
-  #         :key => card.key
-  #       }
-  #       Map.put(game.cards, card.key, new_card)
-  #     end
-  #   else
-  #     game.oneClicked = true
-  #     game.prevCard = card
-  #     new_card = %{
-  #       :value => card.value,
-  #       :state => "revealed",
-  #       :key => card.key
-  #     }
-  #     Map.put(game.cards, card.key, new_card)
-  #   end
-  # end
+  def click_card(game, card) do
+    if game.oneClicked === true do
+      if card.key === prevCard.key do
+        game.oneClicked = false
+        game.prevCard = nil
+        new_card1 = %{
+          :value => card.value,
+          :state => "solved",
+          :key => card.key
+        }
+        new_card2 = %{
+          :value => prevCard.value,
+          :state => "solved",
+          :key => prevCard.key
+        }
+        Map.put(game.cards, card.key, new_card1)
+        Map.put(game.cards, prevCard.key, new_card2)
+      else
+        game.oneClicked = false
+        game.prevCard = nil
+        Task.async(fn -> hide_two_cards(game, card1, card2) end)
+        new_card = %{
+          :value => card.value,
+          :state => "revealed",
+          :key => card.key
+        }
+        Map.put(game.cards, card.key, new_card)
+      end
+    else
+      game.oneClicked = true
+      game.prevCard = card
+      new_card = %{
+        :value => card.value,
+        :state => "revealed",
+        :key => card.key
+      }
+      Map.put(game.cards, card.key, new_card)
+    end
+  end
 end
