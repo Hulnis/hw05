c5d45af41a529becf9bfd85609ee24eb4982a29a
Hulnis <eric.parker13@gmail.com>
2018-02-10 14:12:01 -0500

    redid gen_cards 10

--- lib/memory_game/game.ex
+++ lib/memory_game/game.ex
@@ -31,7 +31,7 @@ defmodule MemoryGame.Game do
     IO.puts("cards2")
     IO.inspect(cards)
     cards = cards
-    |> Map.new(fn card -> {card[:key], card} end)
+    |> Map.new(fn card -> {Map.fetch(card, :key), card} end)
     IO.puts("cards3")
     IO.inspect(cards)
     cards
